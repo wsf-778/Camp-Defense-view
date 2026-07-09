716696e273168b6ca3d832f675fe956b3a06d6b3
俺找素芬 <oncwnuKizgSuAD3g2QH07ioMTNw8@git.weixin.qq.com>
2026-07-09 10:46:57 +0800

    提交

---
+++ cocos-js/meshopt_decoder.wasm-sXpYC2Mb.js
@@ -0,0 +1,10 @@
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var meshopt_decoder_wasm = exports("default", 'assets/meshopt_decoder.wasm-Dt74TiIT.wasm'); /* asset-hash:bedd883e */
+
+		})
+	};
+}));
